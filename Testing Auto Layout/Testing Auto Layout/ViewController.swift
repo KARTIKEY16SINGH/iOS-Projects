ce74eebaae9f07dac06419ebe0970f8820e32b67
Kartikey Singh <kartikeysingh16@gmail.com>
2020-12-23 13:47:57 +0530

    [iOS]  Checking whether StackView will scroll automatically if content is too large than screen

--- Testing Auto Layout/Testing Auto Layout/ViewController.swift
+++ Testing Auto Layout/Testing Auto Layout/ViewController.swift
@@ -8,10 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var stackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        for _ in 1...10 {
+            let button = UIButton()
+            button.backgroundColor = .red
+            button.frame = CGRect(x: button.layer.frame.origin.x, y: button.layer.frame.origin.y, width: 50, height: 50)
+            stackView.addSubview(button)
+        }
     }
 
 
