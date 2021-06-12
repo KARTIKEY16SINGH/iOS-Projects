5a226fdb3d3a59bc195d449be53ff80de6f4c57b
Kartikey Singh <kartikeysingh16@gmail.com>
2021-06-13 00:20:13 +0530

    [Smack Using Pods] Project Setup

--- Smack Chat App/Smack Chat App/ChatViewController.swift
+++ Smack Chat App/Smack Chat App/ChatViewController.swift
@@ -14,6 +14,34 @@ class ChatViewController : UIViewController {
     
     weak var delegate : RevealViewControllerDelegate!
     
+    @IBOutlet weak var navigationBar : UINavigationBar!
+    @IBOutlet weak var navigationBarItem: UINavigationItem!
+    //MARK: VC Life Cycle Function
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setupNavigationView()
+    }
+    
+    private func setupNavigationView() {
+        navigationBarItem.title = "#general"
+        let Image = UIImageView(image: #imageLiteral(resourceName: "smackBurger"))
+        Image.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        let menuButton = UIButton(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+        menuButton.setImage(#imageLiteral(resourceName: "smackBurger"), for: .normal)
+        print("1 Menu Button = \(menuButton)")
+        menuButton.frame = CGRect(x: 0, y: 0, width: 30, height: 20)
+        print("2 Menu Button = \(menuButton)")
+        navigationBarItem.leftBarButtonItem = UIBarButtonItem(customView: menuButton)
+        print("NavigationItem = \(navigationItem)")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: menuButton)
+        print("3 Menu Button = \(menuButton)")
+    }
+    
+    //MARK: Menu Button Action
     @IBAction func revealButtonTapped(_ sender: Any) {
         delegate.showRevealVC()
     }
--- Smack Chat App/Smack Chat App/ViewController.swift
+++ Smack Chat App/Smack Chat App/ViewController.swift
@@ -14,12 +14,28 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.revealVCWidthConstant.constant = 0
+        setupNavigationView()
         // Do any additional setup after loading the view.
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let destinationVC = segue.destination  as? ChatViewController else {return}
         destinationVC.delegate = self
+    }
+    
+    private func setupNavigationView() {
+//        navigationBarItem.title = "#general"
+        let Image = UIImageView(image: #imageLiteral(resourceName: "smackBurger"))
+        Image.frame = CGRect(x: 0, y: 0, width: 30, height: 30)
+        let menuButton = UIButton(frame: CGRect(x: 0, y: 0, width: 30, height: 30))
+        menuButton.setImage(#imageLiteral(resourceName: "smackBurger"), for: .normal)
+        print("1 Menu Button = \(menuButton)")
+        menuButton.layer.frame = CGRect(x: 0, y: 0, width: 30, height: 20)
+        print("2 Menu Button = \(menuButton)")
+//        navigationBarItem.leftBarButtonItem = UIBarButtonItem(customView: menuButton)
+        print("NavigationItem = \(navigationItem)")
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: menuButton)
+        print("3 Menu Button = \(menuButton)")
     }
 }
 
