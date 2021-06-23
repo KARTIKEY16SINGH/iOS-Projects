31b1972657ee89c9d4eb450bb7a9415b2301b4b5
Kartikey Singh <kartikeysingh16@gmail.com>
2021-06-23 22:43:39 +0530

    [Smack Using Pods] Created Login Storyboard GUI

---
+++ Smack Chat App Udemy/Smack Chat App/Smack Chat App/Controllers/LoginViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LoginViewController.swift
+//  Smack Chat App
+//
+//  Created by Iron Man on 23/06/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func dismissButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+        navigationController?.popViewController(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
